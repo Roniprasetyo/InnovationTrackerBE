aba2ee42537c3a7b706e30612ecbc8dc4a910e57
ZymboWannaCode <budiprasetiyo170@gmail.com>
2025-03-17 07:57:28 +0700

    update-qcc qcp

--- innovation-tracker-backend/obj/Debug/net8.0/innovation-tracker-backend.AssemblyInfo.cs
+++ innovation-tracker-backend/obj/Debug/net8.0/innovation-tracker-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("innovation-tracker-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0e9eeb7c966a25930777ea5fceb05d18e175ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c43b02a8e85df6df75e0a8399f925242e90152")]
 [assembly: System.Reflection.AssemblyProductAttribute("innovation-tracker-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("innovation-tracker-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
